19aa96d6d6949bb6f6073120ee196333a8f4c473
Francisco Salazar (Franvierdev) <xxaverii@gmail.com>
2024-02-07 13:04:21 -0400

    portfolio

---
+++ src/app/dashboard/navbar.tsx
@@ -0,0 +1,85 @@
+"use client";
+import React, { useState } from "react";
+
+import Link from "next/link";
+
+export default function Navbar() {
+  let [open, setOpen] = useState(false);
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    open === false ? setOpen(true) : setOpen(false);
+
+    console.log(open);
+  };
+  return (
+    <div className="  flex w-full items-center justify-start ps-2 bg-slate-50/10 sm:bg-transparent sm:mx-auto sm:mt-4 font-mono text-sm">
+      <button onClick={handleSubmit} className="sm:hidden ">
+        {open === false ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 "
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6  absolute z-10 top-1"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
+        )}
+      </button>
+      <div
+        className={
+          open
+            ? `backdrop-blur-sm animate-fade-right max-w-md flex flex-col  w-full min-h-screen items-center justify-around mx-auto `
+            : `backdrop-blur-sm animate-reverse hidden max-w-md shadow-lg shadow-slate-500 rounded-xl sm:flex  w-full items-center justify-around mx-auto`
+        }
+      >
+        <Link
+          className="hover:decoration-slice  text-blue-400 hover:bg-slate-50/30  ease-in duration-300"
+          href="/"
+        >
+          Home
+        </Link>
+        <Link
+          className="hover:underline  text-blue-400 hover:bg-slate-50/30 ease-in duration-300"
+          href="/"
+        >
+          Sobre mi
+        </Link>
+
+        <a
+          className="hover:underline  text-blue-400 hover:bg-slate-50/30 ease-in duration-300"
+          href="#"
+        >
+          Projects
+        </a>
+
+        <a
+          className="hover:underline text-blue-600 hover:text-blue-900   hover:bg-slate-50/60  ease-in duration-300"
+          href="#"
+        >
+          Contáctame
+        </a>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/sections/hero/page.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <div className=" w-[96%] h-screen area ">
+      <ul className="  circles ">
+        <li className="blur-sm hover:blur-0"></li>
+        <li className="blur-sm  hover:blur-0"></li>
+        <li className="blur-sm  hover:blur-0"></li>
+        <li className="blur-sm hover:blur-0"></li>
+        <li className="blur-sm  hover:blur-0"></li>
+        <li className="blur-sm  hover:blur-0"></li>
+        <li className="blur-sm  hover:blur-0"></li>
+        <li className="blur-sm  hover:blur-0"></li>
+        <li className="blur-sm  hover:blur-0"></li>
+        <li className="blur-sm  hover:blur-0"></li>
+        <li className="blur-sm  hover:blur-0"></li>
+        <li className="blur-sm  hover:blur-0"></li>
+        <li className="blur-sm  hover:blur-0"></li>
+      </ul>
+      <div className="flex mt-20 gap-5">
+        <div className=" backdrop-blur-sm  animate-fade-down max-w-xs mx-2 p-5 rounded-3xl min-h-80 border border-slate-100/10  bg-black/10 shadow-xl shadow-slate-500/20  mt-8 flex flex-col items-center gap-2 font-mono ">
+          <Image
+            src="/imgs/perfil.jpg"
+            alt={"asd"}
+            width={808}
+            height={1080}
+            className="rounded-full w-28    mb-2"
+          />
+          <p className="text-lg">Francisco Salazar</p>
+          <p className="text-blue-600 font-bold">Desarrollador Web</p>
+          <p className="text-xs">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta
+            alias ex tenetur nemo sit, dignissimos tempora quos velit quo
+            architecto, quaerat odio. Maxime error dolores cum ad, provident
+            magnam molestias.
+          </p>
+        </div>
+        <div className="  animate-fade-down  mx-2 p-5   mt-8 flex flex-col items-start gap-2 font-mono ">
+          <p className="animate-jump-in animate-duration-1000">I am...</p>
+          <p className="text-4xl text-blue-900/90 font-bold">
+            Francisco Salazar.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
